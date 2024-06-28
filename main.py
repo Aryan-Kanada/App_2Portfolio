e4f49d093352aa6e32d06eb537c42e1cc63ef1e2
aryan <kanadaaryan9@gmail.com>
2024-06-28 19:41:05 +0530

    Add description on new columns

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.image("images\Photo.png",)
+
 with col2:
     st.title("Aryan Kanada")
-    content = "Hi Myself Aryan Kanada"
-    st.info(content)
+    content1 = """
+    Hi Myself Aryan Kanada
+    """
+    st.info(content1)
+
+content2 = """
+Below you can find some of app I have built in Python.Fell free to content me
+"""
+st.text(content2)
+
