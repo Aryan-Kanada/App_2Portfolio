9477042342ce3cb5e094684343b1f0cdbc09a31f
aryan <kanadaaryan9@gmail.com>
2024-06-28 21:29:43 +0530

    Imported data.csv [title]

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -15,7 +15,21 @@ with col2:
     st.info(content1)
 
 content2 = """
-Below you can find some of app I have built in Python.Fell free to content me
+Below you can find some of app I have built in Python. Fell free to content me!
 """
-st.text(content2)
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:11].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[11:].iterrows():
+        st.header(row["title"])
+
+
 
