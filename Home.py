7a680fcacc1eef80174962b028ec03804f1a5329
Aryan-Kanada <kanadaaryan9@gmail.com>
2025-02-05 12:31:19 +0530

    Imahe add to github

--- Home.py
+++ Home.py
@@ -1,11 +1,13 @@
+# To run "streamlit run Home.py"
 import streamlit as st
 import pandas
+
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images\Photo.png",)
+    st.image("images\Photo.png", )
 
 with col2:
     st.title("Aryan Kanada")
@@ -29,7 +31,6 @@ with col3:
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
-
 
 with col4:
     for index, row in df[10:].iterrows():
